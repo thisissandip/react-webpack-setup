f78078e923b1710116ccf4666a40b663d760c96f
thisissandip <thisissandipmondal@gmail.com>
2021-04-28 16:34:36 +0530

    Webpack dependencies and Src Files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+	return <div>Hello From React Webpack Setup</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
