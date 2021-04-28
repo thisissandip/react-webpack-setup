6123acbcb8d91ea1671efd7c4b3934783a0bdc1a
thisissandip <thisissandipmondal@gmail.com>
2021-04-28 16:47:12 +0530

    Development Environment Done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style.scss';
+
+const alertThis = () => {
+	alert('Hey');
+};
 
 const App = () => {
-	return <div>Hello From React Webpack Setup</div>;
+	return (
+		<>
+			<div onClick={() => alertThis()}>Hello World</div>
+			<hr />
+			<div>Image:</div>
+			<img src={require('./assets/Powai.jpeg')} alt='Image' />
+		</>
+	);
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
 	mode: 'development',
@@ -15,9 +16,29 @@ module.exports = {
 				exclude: /node-modules/,
 				loader: 'babel-loader',
 			},
+			{
+				test: /\.scss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.html$/,
+				use: ['html-loader'],
+			},
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				use: {
+					loader: 'file-loader',
+					options: {
+						esModule: false,
+						name: '[name].[hash].[ext]',
+						outputPath: 'imgs',
+					},
+				},
+			},
 		],
 	},
 	plugins: [
+		new CleanWebpackPlugin(),
 		new HTMLWebpackPlugin({
 			template: path.join(__dirname, '/src/index.html'),
 		}),
